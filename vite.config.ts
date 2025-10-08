07abd8a6a0ac2f624a2ad70a3a21d96c1f4c224b
usps-dataeng <casey.y.smith@usps.gov>
2025-10-08 14:51:57 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/api-form/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
